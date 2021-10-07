b62d8da3b20ac6e50209c027dcf9de798714e800
fjdrjr <92042777+fjdrjr@users.noreply.github.com>
2021-10-07 20:07:32 +0200

    Add files via upload

---
+++ finqwerty/src/main/java/de/fjdrjr/titanqwerty/KeyboardLayoutsReceiver.java
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//package fi.onse.qwerty.de.fjdrjr.titanqwerty;
+package de.fjdrjr.titanqwerty;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class KeyboardLayoutsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        /* do nothing, the manifest contains the needed metadata */
+    }
+}
